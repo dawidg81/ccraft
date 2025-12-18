1886514c9e13cdeb26950f3be833a60e89bb25c4
dawidg81 <dawidgorski.m@gmail.com>
2025-12-18 14:47:19 +0100

    Updated game.hpp

--- src/game.hpp
+++ src/game.hpp
@@ -2,6 +2,7 @@
 #define GAME_HPP
 
 #include <string>
+#include <cstdint>
 
 int receiveGameBuffer(int clientSocket);
 
@@ -9,5 +10,7 @@ void sendBlock(int clientSocket, short x, short y, short z, uint8_t block_id);
 void sendAbsolutePosOrt(int clientSocket, uint8_t player_id, uint8_t x[2], uint8_t y[2], uint8_t z[2], uint8_t yaw, uint8_t pitch);
 void sendRelativePosOrt(int clientSocket, uint8_t player_id, uint8_t x, uint8_t y, uint8_t z, uint8_t yaw, uint8_t pitch);
 void sendMessage(int clientSocket, int8_t player_id, std::string message);
+void sendSpawnPlayer(uint8_t targetPlayerId, uint8_t newPlayerId, std::string username, int16_t x, int16_t y, int16_t z, uint8_t yaw, uint8_t pitch);
+void sendDespawnPlayer(uint8_t playerId);
 
 #endif
