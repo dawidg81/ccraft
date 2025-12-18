41a74edb0b7e9cc1aad1fabc974e6b903da0fcc0
dawidg81 <dawidgorski.m@gmail.com>
2025-12-18 15:44:39 +0100

    Implementation of tgread manager in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,78 @@ void heartbeatThread(){
 	}
 }
 
+void handleNewConnection(int clientSocket){
+	uint8_t playerIdBuf[131];
+	ssize_t bytesReceived = recv(clientSocket, playerIdBuf, sizeof(playerIdBuf), 0);
+
+	if(bytesReceived < 130){
+		cout << "Receiving player id failed: excepted 130 bytes but got " << bytesReceived << ". Rejecting new connection.\n";
+		close(clientSocket);
+		return;
+	}
+
+	if(playerIdBuf[0] != 0x00){
+		cout << "Receiving player id failed: expected packet id to be 0x00 but got 0x" << hex << (int)playerIdBuf[0] << dec << ". Rejecting new connection." << endl;
+		close(clientSocket);
+		return;
+	}
+
+	if(playerIdBuf[1] != 0x07){
+		cout << "Receiving player id failed: expected protocol version to be 0x07 but got 0x" << hex << (int)playerIdBuf[1] << dec << ". Rejecting new connection." << endl;
+		close(clientSocket);
+		return;
+	}
+
+	string username = string((char*)&playerIdBuf[2], 64);
+	size_t end = username.find_last_not_of(" ");
+	if(end != string::npos){
+		username = username.substr(0, end + 1);
+	}
+
+	cout << "New connection fully identified. Welcome " << username << " to the server!\n";
+
+	sendServerId(clientSocket, "A Server", "Welcome!");
+	sendLevelInit(clientSocket);
+	sendLevelData(clientSocket);
+	sendLevelFinalize(clientSocket);
+	
+	uint8_t playerId = g_playerManager->addPlayer(clientSocket, username);
+
+	Player* newPlayer = g_playerManager->getPlayer(playerId);
+	if(!newPlayer){
+		cout << "Error: failed to fetch player from threader. Closing new connection.\n";
+		close(clientSocket);
+		return;
+	}
+
+	newPlayer->x = 2048;
+	newPlayer->y = 2112;
+	newPlayer->z = 2048;
+	newPlayer->yaw = 0;
+	newPlayer->pitch = 0;
+
+	{
+		lock_guard<mutex> lock(g_playerManager->playersMutex);
+		for(auto existingPlayer : g_playerManager->players){
+			if(existingPlayer->playerId != playerId && existingPlayer->active){
+				sendSpawnPlayer(playerId, existingPlayer->playerId, existingPlayer->username, existingPlayer->x, existingPlayer->y, existingPlayer->z, existingPlayer->yaw, existingPlayer->pitch);
+			}
+		}
+	}
+
+	{
+		lock_guard<mutex> lock(g_playerManager->playersMutex);
+		for(auto existingPlayer : g_playerManager->players){
+			if(existingPlayer->playerId != playerId && existingPlayer->active){
+				sendSpawnPlayer(existingPlayer->playerId, playerId, username, newPlayer->x, newPlayer->y, newPlayer->z, newPlayer->yaw, newPlayer->pitch);
+			}
+		}
+	}
+
+	newPlayer->playerThread = thread(playerGameLoop, clientSocket, playerId);
+	newPlayer->playerThread.detach();
+}
+
 void connectionThread(int serverSocket){
 	while(running){
 		int clientSocket = accept(serverSocket, nullptr, nullptr);
@@ -36,12 +108,8 @@ void connectionThread(int serverSocket){
 		}
         
 		cout << "New connection accepted" << endl;
-        
-		recvPlayerId(clientSocket);
-		sendServerId(clientSocket, "A Server", "Welcome!");
-		sendLevelInit(clientSocket);
-		sendLevelData(clientSocket);
-		sendLevelFinalize(clientSocket);
+
+		thread(handleNewConnection, clientSocket).detach();
 	}
 }
 
