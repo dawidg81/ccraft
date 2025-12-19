54ca693d181064ea2091017eee6f1a35c4e7608e
dawidg81 <dawidgorski.m@gmail.com>
2025-12-19 18:30:41 +0100

    Syntax fix

--- src/game.hpp
+++ src/game.hpp
@@ -4,10 +4,6 @@
 #include <string>
 #include <cstdint>
 
-void recvBlock(uint8_t senderPlayerId, int16_t x, int16_t y, int16_t z, uint8_t block_id);
-void recvPosOrt(uint8_t playerId, int16_t x, int16_t y, int16_t z, uint8_t yaw, uint8_t pitch);
-void recvMessage(uint8_t senderPlayerId, std::string message);
-
 void sendBlock(uint8_t senderPlayerId, int16_t x, int16_t y, int16_t z, uint8_t block_id);
 void sendAbsolutePosOrt(uint8_t playerId, int16_t x, int16_t y, int16_t z, uint8_t yaw, uint8_t pitch);
 void sendRelativePosOrt(uint8_t playerId, int8_t x, int8_t y, int8_t z, uint8_t yaw, uint8_t pitch);
@@ -15,4 +11,7 @@ void sendMessage(uint8_t senderPlayerId, std::string message);
 void sendSpawnPlayer(uint8_t targetPlayerId, uint8_t newPlayerId, std::string username, int16_t x, int16_t y, int16_t z, uint8_t yaw, uint8_t pitch);
 void sendDespawnPlayer(uint8_t playerId);
 
+void recvBlock(uint8_t senderPlayerId, int16_t x, int16_t y, int16_t z, uint8_t block_id);
+void recvPosOrt(uint8_t playerId, int16_t x, int16_t y, int16_t z, uint8_t yaw, uint8_t pitch);
+void recvMessage(uint8_t senderPlayerId, std::string message);
 #endif
